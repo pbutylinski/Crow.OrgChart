56a96bb16c3cee5a485da18897d0895d3741f40d
Paweł Butyliński <pbutylinski@outlook.com>
2020-01-05 13:06:08 +0100

    Moved JS and CSS to proper folder structure

--- Crow.OrgChart/Startup.cs
+++ Crow.OrgChart/Startup.cs
@@ -2,12 +2,9 @@ using Crow.OrgChart.DataStorage;
 using Crow.OrgChart.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
-using System.IO;
 
 namespace Crow.OrgChart
 {
@@ -20,21 +17,20 @@ namespace Crow.OrgChart
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
             services.AddTransient<IOrganizationStorageRepository, OrganizationStorageRepository>();
-            services.AddTransient<ISearchService, SearchService>();
             services.AddTransient<IOrganizationViewModelService, OrganizationViewModelService>();
+            services.AddTransient<ISearchService, SearchService>();
 
             services.AddMvcCore(options =>
             {
-                options.Filters.Add(new BasicAuthenticationAttribute(Configuration));
+                options.Filters.Add(new BasicAuthenticationAttribute(this.Configuration));
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -44,9 +40,9 @@ namespace Crow.OrgChart
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
@@ -61,24 +57,6 @@ namespace Crow.OrgChart
             });
 
             app.UseStaticFiles();
-
-            app.UseStaticFiles(new StaticFileOptions()
-            {
-                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"Images")),
-                RequestPath = new PathString("/images")
-            });
-
-            app.UseStaticFiles(new StaticFileOptions()
-            {
-                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"Scripts")),
-                RequestPath = new PathString("/scripts")
-            });
-
-            app.UseStaticFiles(new StaticFileOptions()
-            {
-                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"Styles")),
-                RequestPath = new PathString("/styles")
-            });
         }
     }
 }
